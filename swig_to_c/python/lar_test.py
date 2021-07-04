09ead7350f71625f2f998b9bc92094083d3b0cc5
wry <lzavaleta@gmail.com>
2021-07-04 20:31:06 +0200

    Fix swig python example

--- swig_to_c/python/lar_test.py
+++ swig_to_c/python/lar_test.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 
-# PYTHONPATH=$(pwd)/build ./python/hellob_test.py
+import Lar
 
-import hellob
+lar = Lar.Lar_new()
 
-print hellob.hello(42);
+# will crash if path incorrect, should pass a char**
+error = None
+release = Lar.Lar_first_release_item(lar, "/tmp/releases.rss", error)
+
+if release is None:
+    print("Error : $error")
+    # free(error);
+    exit(1)
+
+crelease = Lar.CRelease()
+Lar.Release_copy_into(release, crelease)
+
+print("{}, {}, {}, {}\n".format(crelease.title, crelease.link, crelease.description, crelease.pub_date))
+
+Lar.Release_delete(release)
+
+Lar.Lar_delete(lar)
